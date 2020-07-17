934f18c006fe3a0ab0216322b03ea5b8dc6f0db4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-17 06:42:20 +0000

    Deploy asteffey/fcc-scatterplot-graph to github.com/asteffey/fcc-scatterplot-graph.git:gh-pages

---
+++ precache-manifest.61d0240d0727c3f65d5acfcc85dd174c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45095a34d886cc9bdeb1d27ed25aee3c",
+    "url": "/fcc-scatterplot-graph/index.html"
+  },
+  {
+    "revision": "56649b7c5b4739422fe7",
+    "url": "/fcc-scatterplot-graph/static/css/main.ee0ef14c.chunk.css"
+  },
+  {
+    "revision": "685320c2089a212d56c7",
+    "url": "/fcc-scatterplot-graph/static/js/2.c8b91fd0.chunk.js"
+  },
+  {
+    "revision": "3f06f0f9ff8e6a78c5332d3eed7e4fd4",
+    "url": "/fcc-scatterplot-graph/static/js/2.c8b91fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56649b7c5b4739422fe7",
+    "url": "/fcc-scatterplot-graph/static/js/main.59a89d77.chunk.js"
+  },
+  {
+    "revision": "b8826bd213c9aff8f58c",
+    "url": "/fcc-scatterplot-graph/static/js/runtime-main.da41fe17.js"
+  }
+]);
